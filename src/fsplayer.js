5d7420913b730ddb22d25a339c4e0ca342a061d6
John Yani <vanuan@gmail.com>
2013-07-17 01:33:03 +0300

    Refactor to a separate module

---
+++ src/fsplayer.js
@@ -0,0 +1,54 @@
+(function() {
+  'use strict';
+
+  function BetterFsPlayer() {
+    function createNewPlayer(width, height, mp4link) {
+      var newPlayer = document.createElement("video");
+      var source = document.createElement("source");
+      source.src = mp4link;
+      source.type = "video/mp4";
+      newPlayer.style.width = width;
+      newPlayer.style.height = height;
+      newPlayer.controls = true;
+      newPlayer.volume = 1;
+      newPlayer.autoplay = true;
+      newPlayer.appendChild(source);
+      return newPlayer;
+    }
+
+    function getPlayer() {
+      var player = document.getElementById("player");
+      return player;
+    }
+
+    function removeOldPlayer() {
+      var player = getPlayer();
+      var playerContainer = player.parentNode;
+      playerContainer.removeChild(player);
+    }
+
+    function insertNewPlayer(mp4link) {
+      var player = getPlayer();
+      var width = player.style.width;
+      var height = player.style.height;
+      var playerContainer = player.parentNode;
+      var newPlayer = createNewPlayer(width, height, mp4link);
+      newPlayer.className = "playr_video";
+      playerContainer.appendChild(newPlayer);
+      return newPlayer;
+    }
+
+    function replacePlayer() {
+      var mp4link = FS_FLOWPLAYER_CONFIG.playlist[0].url;
+      var newPlayer = insertNewPlayer(mp4link);
+      removeOldPlayer();
+      var video_objects = [];
+      video_objects.push(new Playr(0, newPlayer));
+    }
+
+    replacePlayer();
+  }
+
+  window.BetterFsPlayer = BetterFsPlayer;
+
+})();
