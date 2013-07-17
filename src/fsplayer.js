cadbe482d3bfe11f438fa70816de27b61d39d8b4
John Yani <vanuan@gmail.com>
2013-07-17 10:19:02 +0300

    Notify the site about track change

--- src/fsplayer.js
+++ src/fsplayer.js
@@ -20,14 +20,19 @@
       var playerContainer = player.parentNode;
       var newPlayerContainer = document.createElement("div");
       playerContainer.appendChild(newPlayerContainer);
-      ShyPlayer(newPlayerContainer, mp4links, width, height);
+      return ShyPlayer(newPlayerContainer, mp4links, width, height);
     }
 
     function replacePlayer() {
+      var urlToClip = {};
       var mp4links = FS_FLOWPLAYER_CONFIG.playlist.map(function (el) {
+        urlToClip[location.protocol + "//" + location.hostname + el.url] = el;
         return el.url;
       });
-      insertNewPlayer(mp4links);
+      var newPlayer = insertNewPlayer(mp4links);
+      newPlayer.addEventListener('loadstart', function() {
+        FS_FLOWPLAYER_CONFIG.player.options.clip.onStart(urlToClip[newPlayer.currentSrc]);
+      });
       removeOldPlayer();
     }
 
